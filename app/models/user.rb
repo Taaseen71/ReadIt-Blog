6688ce26580b73b24ac50c7a017f019180e05563
Saadat Taaseen <taaseen.homework@gmail.com>
2020-06-10 04:46:11 -0400

    changed password_digest to password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 10}
+    validates :email, presence: true, uniqueness: {case_sensitive: false}
+    validates :email, format: {with:  URI::MailTo::EMAIL_REGEXP}
+    validates :password, length: {minimum: 6}
+
 end
