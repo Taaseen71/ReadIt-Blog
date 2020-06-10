bab04cf93f2eb2db40ba1eeda4a59c5342d5883b
Saadat Taaseen <taaseen.homework@gmail.com>
2020-06-10 04:24:11 -0400

    moved files. servers working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "read_it_blog_production"
+  # config.active_job.queue_name_prefix = "ReadIt_Blog_production"
 
   config.action_mailer.perform_caching = false
 
