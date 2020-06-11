07978451ca12503c5de1762ac8995870f1b2b234
Saadat Taaseen <taaseen.homework@gmail.com>
2020-06-11 00:21:31 -0400

    renders all articles, and comments. Doesnt render username associated with it

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, Component } from 'react';
+import axios from 'axios';
+// import { Route, Switch, Link } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Hello World!</h1>
-        </div >
-    );
+
+class App extends Component {
+    state = {
+        data: [],
+        users: []
+    }
+
+
+
+    async componentDidMount() {
+        console.log("componentdidmount");
+        let data = await axios("http://localhost:3000/articles/");
+        data = data.data
+        this.setState({ data });
+        console.log("Data:", this.state.data);
+
+
+        console.log("componentDidMount2");
+        let users = await axios("http://localhost:3000/users")
+        users = users.data
+        this.setState({ users })
+        console.log("users:", this.state.users[0].username)
+    }
+
+
+
+
+
+
+
+    render() {
+        // let items = this.state.data.map(item =>
+        //     <>
+        //         <h5> <span>{item.id}.</span> {item.title}</h5>
+        //         <p> {item.description}</p>
+        //     </>
+        // )
+
+
+        return (
+            <div className="App">
+                <h1>Hello</h1>
+                <div>{this.state.data.map(item =>
+                    <>
+                        <h5> <span>{item.id}.</span> {item.title}</h5>
+                        {/* <h6> {this.state.users[0].username}</h6> */}
+
+                        <p> {item.description}</p>
+                        <h6>Commments:</h6>
+                        <div>
+                            {item.comments.map(parameter => {
+                                return (
+                                    <>
+                                        <p>{parameter.my_comment}</p>
+                                        <p>{parameter.user_id}</p>
+                                        {/* <p>{x[parameter.user_id]}</p> */}
+                                    </>
+                                )
+                            })}
+
+                        </div>
+                    </>
+                )}</div>
+                {/* this also works */}
+                {/* <div>
+                    {items}
+                </div> */}
+            </div>
+        )
+    }
 }
 
 export default App;
