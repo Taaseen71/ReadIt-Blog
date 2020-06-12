bba83e99c60dfe6ac432df913752bb8c03673bfa
Saadat Taaseen <taaseen.homework@gmail.com>
2020-06-12 15:34:53 -0400

    changed api-helper.js

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3000';
+// const baseUrl = 'http://localhost:3000';
+
+const baseUrl = process.env.NODE_ENV === 'production'
+    ?
+    // /* link to your heroku app. Example:*/'https://school-app-test.herokuapp.com/'
+    'https://read-it-api-saad.herokuapp.com/'
+    :
+    'http://localhost:3000'
+
 
 const api = axios.create({
     baseURL: baseUrl
